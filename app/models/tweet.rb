c066644d8309abb67d734bb451149ada730c44ea
Andrés Howard <ahowardm@gmail.com>
2023-06-10 17:49:06 -0400

    feat: crear tags al crear un tweet

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,6 +2,18 @@ class Tweet < ApplicationRecord
     belongs_to :user
     has_many :tags
 
+    attribute :hashtags
+
+    after_create :create_tags
+
+    def create_tags
+        # debugger
+        hashtags.split(',').each do |h|
+            hashtag = Hashtag.find_or_create_by name: h
+            Tag.create tweet: self, hashtag: hashtag
+        end
+    end
+
     def creation_date
         created_at.strftime("%e %b %Y")
     end
