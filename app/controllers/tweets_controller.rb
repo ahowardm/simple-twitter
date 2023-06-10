d1f43a54df1564b5109fed31ba8a6a3ec5dde858
Andrés Howard <ahowardm@gmail.com>
2023-06-10 16:29:16 -0400

    feat: agregar destroy a tweets controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -21,6 +21,12 @@ class TweetsController < ApplicationController
         end
     end
 
+    def destroy
+        tweet = Tweet.find params[:id]
+        tweet.destroy
+        redirect_to tweets_path, notice: 'Tweet eliminado con éxito'
+    end
+
     private
     def tweet_params
         params.require(:tweet).permit(:body)
