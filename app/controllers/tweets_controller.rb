a313f7b33a6b73b914fb66860d4d9067bff65788
Andrés Howard <ahowardm@gmail.com>
2023-06-10 16:17:11 -0400

    feat: agregar accion create en tweets controller

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -10,4 +10,19 @@ class TweetsController < ApplicationController
     def new
         @tweet = Tweet.new
     end
+
+    def create
+        tweet = Tweet.new tweet_params
+        tweet.user = current_user
+        if tweet.save
+            redirect_to tweet, notice: 'Tweet guardado con éxito'
+        else
+            render :new
+        end
+    end
+
+    private
+    def tweet_params
+        params.require(:tweet).permit(:body)
+    end
 end
